317fbfab0e7054b946107b6d8f86842b23104cd2
jhkim0759 <jhkim0759@naver.com>
2021-05-02 15:38:54 +0900

    변수제거/변환시도'

--- Preprocessor.py
+++ Preprocessor.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 class Preprocessor():
     def __init__(self):
@@ -7,24 +8,27 @@ class Preprocessor():
     def preprocessing(self, dir):
         df = pd.read_csv(dir)
 
-        df.drop(columns="index", axis=1, inplace=True)
-        df.drop('FLAG_MOBIL', axis=1, inplace=True)
+        drop_df_candidate = df[(df['DAYS_EMPLOYED']) > 0].index
+        df.drop(drop_df_candidate, axis=0, inplace=True)
 
-        df = control_outliner(df)
+        drop_columns = ['index', 'FLAG_MOBIL', 'child_num', 'occyp_type']
+        df.drop(columns=drop_columns, axis=1, inplace=True)
+
         df = dummy_object(df)
-        df = encode_date(df)
-        df = encode_columns(df)
+        df = encode_YN(df)
+        df = encode_all_order_type(df)
+
         return df
 
     def preprocessing_for_analysis(self, dir):
         df = pd.read_csv(dir)
 
-        df.drop(columns="index", axis=1, inplace=True)
-        df.drop('FLAG_MOBIL', axis=1, inplace=True)
+        drop_columns = ['index', 'FLAG_MOBIL']
+        df.drop(columns=drop_columns, axis=1, inplace=True)
 
         df = control_outliner(df)
         df = encode_date(df)
-        df = encode_columns(df)
+        df = encode_YN(df)
         return df
 
 def control_outliner(df):
@@ -35,39 +39,75 @@ def control_outliner(df):
     return df
 
 def dummy_object(df):
-    object_variables = ["family_type", "house_type", "income_type", "edu_type", "occyp_type"]
+    object_variables = ["house_type", "income_type"]
 
     dummied_df = pd.get_dummies(df[object_variables])
     concat_df  = pd.concat([df, dummied_df], axis=1)
     concat_df  = concat_df.drop(columns=object_variables)
     return concat_df
 
-def encode_date(df):
-    df['DAYS_BIRTH']    = round(df['DAYS_BIRTH']    / -365)
-    df['DAYS_EMPLOYED'] = round(df['DAYS_EMPLOYED'] / -365)
-
-    df['begin_month'] = df['begin_month'] * -1
-
-    df.loc[df['DAYS_EMPLOYED'] >= 35, 'DAYS_EMPLOYED'] = 35
-    df.loc[df['DAYS_EMPLOYED'] < 0, 'DAYS_EMPLOYED'] = -1
-
-    return df
-
-def encode_columns(df):
+def encode_YN(df):
     df['reality'] = df['reality'].apply(encode_yes_no)
     df['gender'] = df['gender'].apply(encode_gender)
     df['car'] = df['car'].apply(encode_yes_no)
 
     return df
 
-def encode_gender(index):
-    if index == 'M' or index == 1:
+def encode_gender(item):
+    if item == 'M' or item == 1:
         return 1
     else:
         return 0
 
-def encode_yes_no(index):
-    if index == 'Y' or index == 1:
+def encode_yes_no(item):
+    if item == 'Y' or item == 1:
         return 1
     else:
         return 0
+
+def encode_all_order_type(df) :
+    order_variables = ["family_type", "edu_type"]
+
+    for order_variable in order_variables:
+        df[order_variable] = df[order_variable].apply(encode_order_type)
+    return df
+
+def encode_order_type(item):
+    types = ['Commercial associate', 'Working', 'State servant', 'Pensioner', 'Student']
+    if item in types:
+        return types.index(item)
+
+    types = ['Higher education' ,'Secondary / secondary special', 'Incomplete higher', 'Lower secondary', 'Academic degree']
+    if item in types:
+        return types.index(item)
+
+    types = ['Married', 'Civil marriage', 'Separated', 'Single / not married', 'Widow']
+    if item in types:
+        return types.index(item)
+
+    types = ['Municipal apartment', 'House / apartment', 'With parents', 'Co-op apartment', 'Rented apartment', 'Office apartment']
+    if item in types:
+        return types.index(item)
+
+    types = ['Municipal apartment', 'House / apartment', 'With parents', 'Co-op apartment', 'Rented apartment',
+             'Office apartment']
+    if item in types:
+        return types.index(item)
+
+def remove_outlier(train, column):
+    df = train[column]
+    # 1분위수
+    quan_25 = np.percentile(df.values, 25)
+
+    # 3분위수
+    quan_75 = np.percentile(df.values, 75)
+
+    iqr = quan_75 - quan_25
+
+    lowest = quan_25 - iqr * 1.5
+    highest = quan_75 + iqr * 1.5
+    outlier_index = df[(df < lowest) | (df > highest)].index
+    print('outlier의 수 : ', len(outlier_index))
+    train.drop(outlier_index, axis=0, inplace=True)
+
+    return train
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Credit_card_DACON
 https://dacon.io/competitions/official/235713/overview/description/
+
+
+#### family type , edu order
+![image.png](attachment:image.png)
+
+최소값으로 정규화한 결과는 동일 
+
+#### employed_date 0이상을 0으로 바꿨을 때
+![image-2.png](attachment:image-2.png)
+비슷함
+
+#### family type을 더미할 때 
+![image-3.png](attachment:image-3.png)
+살짝 떨어짐
+
+#### employ_date 0이상인 값 drop 하과 다시 돌릴 때 
+![image-4.png](attachment:image-4.png)
