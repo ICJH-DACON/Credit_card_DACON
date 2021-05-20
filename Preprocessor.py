0b44cf020c13704ebb85853f92d4e958ab03640f
jhkim0759 <jhkim0759@naver.com>
2021-05-20 22:06:54 +0900

    pycaret

--- Preprocessor.py
+++ Preprocessor.py
@@ -9,14 +9,17 @@ class Preprocessor():
         df = pd.read_csv(dir)
 
         drop_df_candidate = df[(df['DAYS_EMPLOYED']) > 0].index
-        df.drop(drop_df_candidate, axis=0, inplace=True)
+        df.loc[drop_df_candidate, 'DAYS_EMPLOYED'] = 0
 
         drop_columns = ['index', 'FLAG_MOBIL', 'child_num', 'occyp_type']
         df.drop(columns=drop_columns, axis=1, inplace=True)
 
-        df = dummy_object(df)
-        df = encode_YN(df)
+        object_variables = ["house_type", "income_type"]
+
         df = encode_all_order_type(df)
+        # df = dummy_object(df, object_variables)
+
+        df = encode_YN(df)
 
         return df
 
@@ -38,9 +41,7 @@ def control_outliner(df):
 
     return df
 
-def dummy_object(df):
-    object_variables = ["house_type", "income_type"]
-
+def dummy_object(df, object_variables):
     dummied_df = pd.get_dummies(df[object_variables])
     concat_df  = pd.concat([df, dummied_df], axis=1)
     concat_df  = concat_df.drop(columns=object_variables)
@@ -66,33 +67,21 @@ def encode_yes_no(item):
         return 0
 
 def encode_all_order_type(df) :
-    order_variables = ["family_type", "edu_type"]
+    df["edu_type"] = df["edu_type"].apply(encode_edu_type)
+    df["family_type"] = df["family_type"].apply(encode_family_type)
 
-    for order_variable in order_variables:
-        df[order_variable] = df[order_variable].apply(encode_order_type)
     return df
 
-def encode_order_type(item):
-    types = ['Commercial associate', 'Working', 'State servant', 'Pensioner', 'Student']
-    if item in types:
-        return types.index(item)
-
-    types = ['Higher education' ,'Secondary / secondary special', 'Incomplete higher', 'Lower secondary', 'Academic degree']
-    if item in types:
-        return types.index(item)
-
+def encode_family_type(item):
     types = ['Married', 'Civil marriage', 'Separated', 'Single / not married', 'Widow']
-    if item in types:
-        return types.index(item)
 
-    types = ['Municipal apartment', 'House / apartment', 'With parents', 'Co-op apartment', 'Rented apartment', 'Office apartment']
-    if item in types:
-        return types.index(item)
+    return types.index(item)
 
-    types = ['Municipal apartment', 'House / apartment', 'With parents', 'Co-op apartment', 'Rented apartment',
-             'Office apartment']
-    if item in types:
-        return types.index(item)
+def encode_edu_type(item):
+    types = ['Higher education', 'Secondary / secondary special', 'Incomplete higher', 'Lower secondary',
+             'Academic degree']
+
+    return types.index(item)
 
 def remove_outlier(train, column):
     df = train[column]
@@ -107,7 +96,6 @@ def remove_outlier(train, column):
     lowest = quan_25 - iqr * 1.5
     highest = quan_75 + iqr * 1.5
     outlier_index = df[(df < lowest) | (df > highest)].index
-    print('outlier의 수 : ', len(outlier_index))
     train.drop(outlier_index, axis=0, inplace=True)
 
     return train
--- README.md
+++ README.md
@@ -1,21 +1,44 @@
 # Credit_card_DACON
 https://dacon.io/competitions/official/235713/overview/description/
 
-
+<hr>
 #### family type , edu order
 0.7428531031, DACON점수
 
 최소값으로 정규화한 결과는 동일 
-
+<hr>
 #### employed_date 0이상을 drop으로 바꿨을 때 
 0.7438235671, DACON점수
 비슷함
-
+<hr>
 #### family type을 더미할 때 
 ![image-3.png](attachment:image-3.png)
 살짝 떨어짐
-
+<hr>
 #### 변수 후진제거 했을 때(과적합)
 1.1531252015	
+
+<hr>
+
+### pycaret 모델 생성 
+**preprocessor 
+'''
+    def preprocessing(self, dir):
+        df = pd.read_csv(dir)
+
+        drop_df_candidate = df[(df['DAYS_EMPLOYED']) > 0].index
+        df.loc[drop_df_candidate, 'DAYS_EMPLOYED'] = 0
+
+        drop_columns = ['index', 'FLAG_MOBIL', 'child_num', 'occyp_type']
+        df.drop(columns=drop_columns, axis=1, inplace=True)
+
+        df = encode_all_order_type(df) #edu_type, family_type
+        df = encode_YN(df)
+
+        return df
+'''
+
+> https://blog.naver.com/dalgoon02121/222340409616 보고 참고
+> pycaret 사용법(가상환경 설정) https://pycaret.org/install/
 
 
